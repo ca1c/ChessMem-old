40200c520e48f778830c4dd5687f3eeaedcaa446
Talon Bragg <talonbragg@Talons-MacBook-Air.local>
2020-12-27 20:49:57 -0800

    added a changeOrder method to compareObj.js

--- src/lib/util/compareObj.js
+++ src/lib/util/compareObj.js
@@ -7,6 +7,59 @@ function keyNum(k1, k2) {
 }
 
 /*
+  Step 2: put objects in the correct order
+*/
+
+function changeOrder(o1, o2) {
+  /* This will check that all keys are equal or exist also
+   * The first object's keys will look like this in array form: [a, b, c, d]
+   * The next object's keys will probably be out of order relative to the first object's keys: [b, d, a, c]
+   * The iterator will start on the first key of the first array and find it in the second, and create a new object in order
+   * 
+   * Example of first iteration:
+   * 
+   * (values of each key will obviously come with the key)
+   * 
+   *     ▼
+   *    [a, b, c, d]
+   *      
+   *           ▼
+   *    [b, d, a, c]
+   * 
+   *    new:
+   *    [a]
+   * 
+   */
+
+  let newObj = new Object();
+  let equal = false;
+  
+  let baseObjKeys = Object.keys(o1);
+
+  let returnObj = {
+    newObj: "",
+    equal: equal
+  }
+
+  for(let i = 0; i < baseObjKeys.length; i++) {
+    if(typeof o2[baseObjKeys[i]] === 'undefined') {
+      return returnObj;
+    } else {
+      newObj[baseObjKeys[i]] = o2[baseObjKeys[i]];
+    }
+  }
+
+  equal = true;
+
+  returnObj = {
+    newObj: newObj,
+    equal: equal
+  }
+  
+  return returnObj;
+}
+
+/*
   Step 2: Check if all the keys are equal
 */
 
@@ -43,10 +96,11 @@ export default function comapreObj (o1, o2) {
   let k2 = Object.getOwnPropertyNames(o2);
 
   let keyNumB = keyNum(k1, k2);
-  let keyEquB = keyEqu(k1, k2);
-  let valEquB = valEqu(o1, o2, k1);
+  let changeOrderB = changeOrder(o1, o2);
+  // let keyEquB = keyEqu(k1, k2);
+  let valEquB = valEqu(o1, changeOrderB.newObj, k1);
 
-  if(keyNumB === true && keyEquB === true && valEquB === true) {
+  if(keyNumB === true && changeOrderB.equal === true && valEquB === true) {
     return true;
   }
 
